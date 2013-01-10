f09f20b3fd5975b7cf2c710cd630bed2467c60f9
Ashwini Kumar Singh Chandel <chandelashwini@gmail.com>
2013-01-10 15:33:20 +0530

    done changes

--- gen/com/example/steaming/R.java
+++ gen/com/example/steaming/R.java
@@ -19,40 +19,22 @@ public final class R {
         public static final int stop=0x7f020005;
     }
     public static final class id {
-        public static final int linearLayout1=0x7f060002;
-        public static final int playtogglebutton=0x7f060004;
-        public static final int progressBar=0x7f060003;
-        public static final int relativeLayout1=0x7f060000;
-        public static final int stop=0x7f060005;
-        public static final int textView1=0x7f060006;
-        public static final int textView3=0x7f060008;
-        public static final int timeElapsed=0x7f060007;
-        public static final int timeEnd=0x7f060009;
-        public static final int videoviewer=0x7f060001;
+        public static final int linearLayout1=0x7f050002;
+        public static final int linearLayout2=0x7f050004;
+        public static final int playtogglebutton=0x7f050005;
+        public static final int progressBar=0x7f050003;
+        public static final int relativeLayout1=0x7f050000;
+        public static final int stop=0x7f050006;
+        public static final int textView1=0x7f050007;
+        public static final int textView3=0x7f050009;
+        public static final int timeElapsed=0x7f050008;
+        public static final int timeEnd=0x7f05000a;
+        public static final int videoviewer=0x7f050001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-    }
-    public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-         */
-        public static final int AppBaseTheme=0x7f050000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f050001;
     }
 }
